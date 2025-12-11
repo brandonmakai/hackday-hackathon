aabe34745cac572a043302cd25d34e47a32202eb
Brandon Williams <brandonmakaiw@gmail.com>
2025-12-11 09:37:12 -0800

    refactor frontend to use nextjs
    create initial file setup and install shadcn

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders website analyzer heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Website Analyzer/i);
-  expect(headingElement).toBeInTheDocument();
-});
